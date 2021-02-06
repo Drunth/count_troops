5087f2d087e8307f932ac765a32675cb3f62c613
andreas.fian@logmedia.at <andreas.fian@logmedia.at>
2021-02-06 18:02:10 +0100

    update

--- ut_troop_count.js
+++ ut_troop_count.js
@@ -1,4 +1,4 @@
-ScriptAPI.register('100-UnterstÃ¼tzung zÃ¤hlen', true, 'Ademes', 'support-nur-im-forum@ademes.at')
+ScriptAPI.register('100-Unterstützung zählen', true, 'Ademes', 'support-nur-im-forum@ademes.at')
 Scriptversion = 'MIT-Lizenz - Copyright (c) 2013 Ademes , Version 1.0'
 if (top.frames.length > 1) {
   var doc = (top.frames[1].document.URL.match('game.php') == 'game.php') ? top.frames[1].document : top.frames[0].document
@@ -6,7 +6,7 @@ if (top.frames.length > 1) {
   var doc = document
 };
 if (!doc.URL.match('mode=units')) {
-  UI.InfoMessage('Du musst dich auf der Verteidigungs-/UnterstÃ¼tzungs-Ãœbersicht befinden!', 3000, true)
+  UI.InfoMessage('Du musst dich auf der Verteidigungs-/Unterstützungs-Übersicht befinden!', 3000, true)
 } else {
   ADS_Unterstuetzung_zaehlen(doc)
 };
@@ -17,11 +17,10 @@ function ADS_Unterstuetzung_zaehlen (doc) {
   var num_sword = 1
   var num_archer = (world ? 3 : -999)
   var num_spy = (world ? 4 : 3)
-  var num_marcher = (world ? 6 : -999)
   var num_heavy = (world ? 7 : 5)
-  var num_catapult = (world ? 9 : 7)
   var sum_spear = 0
   var sum_sword = 0
+  var sum_archer = 0
   var sum_heavy = 0
   var sum_spy = 0
   var obj = new Object()
@@ -39,18 +38,14 @@ function ADS_Unterstuetzung_zaehlen (doc) {
         count.sword = 0
         count.archer = 0
         count.spy = 0
-        count.marcher = 0
         count.heavy = 0
-        count.catapult = 0
         obj[acc] = count
       }
       count.spear += parseInt($(this).find('td.unit-item:eq(' + num_spear + ')').html())
       count.sword += parseInt($(this).find('td.unit-item:eq(' + num_sword + ')').html())
       if (world) count.archer += parseInt($(this).find('td.unit-item:eq(' + num_archer + ')').html())
       count.spy += parseInt($(this).find('td.unit-item:eq(' + num_spy + ')').html())
-      if (world) count.marcher += parseInt($(this).find('td.unit-item:eq(' + num_marcher + ')').html())
       count.heavy += parseInt($(this).find('td.unit-item:eq(' + num_heavy + ')').html())
-      count.catapult += parseInt($(this).find('td.unit-item:eq(' + num_catapult + ')').html())
       obj[acc] = count
     }
   })
@@ -58,7 +53,7 @@ function ADS_Unterstuetzung_zaehlen (doc) {
     output += '<h5>Stammesdeff eingesetzt bei:</h5>'
     word = 'Eingesetzt'
   } else if (doc.URL.match('support_detail')) {
-    output += '<h5>UnterstÃ¼tzungen erhalten von:</h5>'
+    output += '<h5>Unterstützungen erhalten von:</h5>'
     word = 'Erhalten'
   }
   var counter = 0
@@ -66,10 +61,14 @@ function ADS_Unterstuetzung_zaehlen (doc) {
   var color = ''
   troops = (typeof (Truppenanzahl) === 'undefined') ? 20000 : Truppenanzahl
 
-  output += '<th style="padding:4px;">Account</th><th style="padding:4px;">Speer</th><th style="padding:4px;">Schwert</th><th style="padding:4px;">Skav</th style="padding:4px;"><th style="padding:4px;">Spy</th>'
+  output += '<th style="padding:4px;">Account</th>'
+  output += '<th style="padding:4px;">Speer</th>'
+  output += '<th style="padding:4px;">Schwert</th>'
+  if (world) output += '<th style="padding:4px;">Bogen</th>'
+  output += '<th style="padding:4px;">Skav</th>'
+  output += '<th style="padding:4px;">Spy</th>'
   $.each(obj, function (acc, count) {
     counter++
-    deffs = Math.round(((count.spear + count.sword + count.archer + 2 * count.spy + 5 * count.marcher + 6 * count.heavy + 8 * count.catapult) / troops) * 10) / 10
 
     if (counter % 2) {
       color = '#f0e2be'
@@ -81,13 +80,14 @@ function ADS_Unterstuetzung_zaehlen (doc) {
     output += '<td style="padding:2px; border-right: solid 1px;"><span style="color:#603000;font-weight:bold">' + acc + '</span></td>'
     output += '<td style="padding:2px;">' + count.spear + '</td>'
     output += '<td style="padding:2px;">' + count.sword + '</td>'
+    if (world) output += '<td style="padding:2px;">' + count.archer + '</td>'
     output += '<td style="padding:2px;">' + count.heavy + '</td>'
     output += '<td style="padding:2px;">' + count.spy + '</td>'
 
     output += '</tr>'
-    sum += Math.round(deffs * 10) / 10
     sum_spear += Math.round(count.spear / troops * 10) / 10
     sum_sword += Math.round(count.sword / troops * 10) / 10
+    sum_archer += Math.round(count.archer / troops * 10) / 10
     sum_heavy += Math.round(count.heavy / troops * 10) / 10
     sum_spy += Math.round(count.spy / troops * 10) / 10
   })
@@ -98,6 +98,7 @@ function ADS_Unterstuetzung_zaehlen (doc) {
     output += '<br><div style="color: green; font-weight: bold;">Insgesamt: <br/>'
     output += '<span style="margin-left:10px;">Speer: ' + sum_spear + '</span> <br/>'
     output += '<span style="margin-left:10px;">Schwert: ' + sum_sword + '</span> <br/>'
+    if (world) output += '<span style="margin-left:10px;">Bogen: ' + sum_archer + '</span> <br/>'
     output += '<span style="margin-left:10px;">Skav: ' + sum_heavy + '</span> <br/>'
     output += '<span style="margin-left:10px;">Spy: ' + sum_spy + '</span></div>'
     output += '<span style="color: grey;">(Anzeige pro Seite)</span>'
